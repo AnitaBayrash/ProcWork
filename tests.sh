a7257e99fde70d762040c9ee3feab0bdfde19647
AnitaBayrash <anitikki@list.ru>
2015-04-07 23:15:59 +0300

    Tests added.

---
+++ tests.sh
@@ -0,0 +1,25 @@
+sudo insmod ./proc.ko
+echo "10*10 test:"
+echo 10 > /proc/first_operand_proc
+echo 10 > /proc/second_operand_proc
+echo '*' > /proc/operation_proc
+sudo cat /proc/result_proc
+sudo rmmod ./proc.ko
+echo "20+30 test:"
+echo 20 > /proc/first_operand_proc
+echo 30 > /proc/second_operand_proc
+echo '+' > /proc/operation_proc
+sudo cat /proc/result_proc
+sudo rmmod ./proc.ko
+echo "50-30 test:"
+echo 50 > /proc/first_operand_proc
+echo 30 > /proc/second_operand_proc
+echo '-' > /proc/operation_proc
+sudo cat /proc/result_proc
+sudo rmmod ./proc.ko
+echo "81/9 test:"
+echo 81 > /proc/first_operand_proc
+echo 9 > /proc/second_operand_proc
+echo '/' > /proc/operation_proc
+sudo cat /proc/result_proc
+sudo rmmod ./proc.ko
